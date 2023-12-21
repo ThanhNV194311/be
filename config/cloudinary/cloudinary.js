bb58627d50b2c870e25a90a08be03961dd14cfca
ThinhTran <thinhtran3803@gmail.com>
2023-12-21 10:02:14 +0700

    123

--- config/cloudinary/cloudinary.js
+++ config/cloudinary/cloudinary.js
@@ -1,12 +1,13 @@
-import dotenv from 'dotenv'
-import cloudinary from 'cloudinary'
+import dotenv from "dotenv";
+import cloudinary from "cloudinary";
 
-dotenv.config()
+dotenv.config();
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+cloudinary.v2.config({
+	cloud_name: "dffigrxdq",
+	api_key: "583957933292513",
+	api_secret: "-R4ukayZLRLdmZtv2wu9EV0B6YE",
+	secure: true,
 });
 
-export default cloudinary
+export default cloudinary;
